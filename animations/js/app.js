a724d763ca8f98b2e8a26da3e0fe0f88e090431d
zoe3351 <jd62@uw.edu>
2017-02-18 16:11:43 -0800

    Altering the Page Challenge

--- animations/js/app.js
+++ animations/js/app.js
@@ -13,12 +13,27 @@
  * - the <button id="animate-button"> element
  */
 
+var nameInput = document.querySelector("#name-input");
+var nameOutput = document.querySelector(".name-output");
+var animationSelect = document.querySelector("#animation-select");
+var animateButton = document.querySelector("#animate-button");
+
 /**
  * add an event listener for the "input" event on the name-input.
  * whenever that event occurs, update the text content of the name-output
  * to be "Hello, " + the value of the input.
  * if the input value is zero-length, just clear the text content
  */
+
+nameInput.addEventListener("input", function(){
+    var text = nameInput.value;
+    if (text.length == 0){
+        nameOutput.textContent = "";
+    }else{
+        nameOutput.textContent = "Hello, " + text;
+    }
+});
+
 
 /**
  * add an event listener for the "click" event on the animate button.
@@ -31,3 +46,12 @@
  * that way, when you add the animation name to the class list again, 
  * the animation will occur.
  */
+
+animateButton.addEventListener("click", function(){
+    var selected = animationSelect.options[animationSelect.selectedIndex].value;
+    nameOutput.classList.add(selected);
+
+    nameOutput.addEventListener("animationend", function(){
+        nameOutput.classList.remove(selected);
+    }, {once: true});
+});
