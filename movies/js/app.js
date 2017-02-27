40ba9528dca8133f4e9d896666ea8ef155485b21
zoe3351 <jd62@uw.edu>
2017-02-26 17:27:50 -0800

    Creating Content from Data Challenge

--- movies/js/app.js
+++ movies/js/app.js
@@ -16,6 +16,12 @@
  */
 function renderMovieProp(value, nonNumeric) {
     //create, populate, and return a new <td> element
+    var td = document.createElement("td");
+    td.textContent = value;
+    if (nonNumeric){
+        td.classList.add("mdl-data-table__cell--non-numeric");
+    }
+    return td;
 }
 
 /**
@@ -25,14 +31,23 @@ function renderMovieProp(value, nonNumeric) {
  * @param {Object} movie the movie object
  * @returns {Object} a new, populated <tr> element 
  */
+
 function renderMovie(movie) {
     //create <tr> element
+    var tr = document.createElement("tr");
 
     //use renderMovieProp() to create <td> elements for
     //the title, sales, and tickets props and append them
     //to the <tr> element 
-
+    var td1 = renderMovieProp(movie.title, true);
+    tr.appendChild(td1);
+    var td2 = renderMovieProp(movie.sales);
+    tr.appendChild(td2);
+    var td3 = renderMovieProp(movie.tickets);
+    tr.appendChild(td3);
+    
     //return the <tr> element
+    return tr;
 }
 
 /**
@@ -43,13 +58,36 @@ function renderMovie(movie) {
  */
 function renderMovies(movies) {
     //clear any existing content within the <tbody> element
+    var tbody = document.querySelector("tbody");
+    tbody.textContent = "";
 
     //for each element in the movies array
 
     //use renderMovie() to create a full-populated <tr> element
     //and append that to the <tbody> elemnet already in the page
+    movies.forEach(function(m){
+        var tr = renderMovie(m);
+        tbody.appendChild(tr);
+    });
 }
 
 //render all of the movies
+MOVIES.sort(function(record1, record2){
+    return -record1.sales+record2.sales;
+});
+
 renderMovies(MOVIES);
+
+var userInput = document.querySelector(".mdl-textfield__input");
+userInput.addEventListener("input", function() {
+    var text = userInput.value.toLowerCase();
+    var filtered = MOVIES.filter(function(m){
+        var title = m.title.toLowerCase();
+        if (title.indexOf(text) != -1){
+            return true;
+        }
+        return false;
+    });
+    renderMovies(filtered);
+});
 
